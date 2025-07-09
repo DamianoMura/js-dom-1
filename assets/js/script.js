d0a805be10647f181c555ff4040f845c99b305e9
DamianoMura <mura.damiano14@gmail.com>
2025-07-09 21:39:10 +0200

    [upd] last details added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@
 //identifico il bottone
 const button=document.getElementById("toggle-light");
 //fase2
-//gli assegno un event listener
+//gli assegno un event listener per click
 button.addEventListener(`click`,function(){
-  let toggle=document.getElementById("light-bulb");
-  if(toggle.src.includes("white")){
+  let toggle=document.getElementById("light-bulb");//acquisiamo l'immagine
+  if(toggle.src.includes("white")){ //se nella url dell'immagine c'è scritto white lo cambiamo con yellow
     toggle.src=toggle.src.replace("white","yellow")
   }
-  else{
+  else{//oppure cambiamo yellow con white
     toggle.src=toggle.src.replace("yellow","white")
   }
 })
