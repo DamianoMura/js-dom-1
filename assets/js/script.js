55140f075240c0d371880deaa6b5ea91c9882aa6
DamianoMura <mura.damiano14@gmail.com>
2025-07-10 01:43:12 +0200

    [upd]  finalised all comments . finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,21 @@ const button=document.getElementById("toggle-light");
 //gli assegno un event listener per click
 button.addEventListener(`click`,function(){
   let toggle=document.getElementById("light-bulb");//acquisiamo l'immagine
-  let button=document.getElementById("toggle-light");
-  if(toggle.src.includes("white")){ //se nella url dell'immagine c'è scritto white lo cambiamo con yellow
+  let button=document.getElementById("toggle-light");//aquisimo il bottone
+  if(toggle.src.includes("white")){ //se nella url dell'immagine c'è scritto white...
+    //rimpiazziamo white con yellow nella stringa dell'attributo src("assets/img/white_lamp.png") dell tag <img> 
     toggle.src=toggle.src.replace("white","yellow")
     //cambiamo accendi in spegni all'interno del bottone
     button.textContent=button.textContent.replace("accendi","spegni");
-    //extra : cambiamo il colore del pulsante
+    //extra : cambiamo il colore del pulsante  da verde a rosso (bootstrap)
     button.className=button.className.replace("success","danger");
     
   }
-  else{//oppure cambiamo yellow con white
+  else{//oppure : cambiamo yellow con white
     toggle.src=toggle.src.replace("yellow","white")
     //cambiamo spegni in accendi all'interno del bottone
     button.textContent=button.textContent.replace("spegni","accendi");
-     //extra : cambiamo il colore del pulsante
+     //extra : cambiamo il colore del pulsante da rosso a verde (bootstrap)
     button.className=button.className.replace("danger","success");
     
   }
