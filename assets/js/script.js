09555ed0203e28cf988b23b43cc85412977c5a46
DamianoMura <mura.damiano14@gmail.com>
2025-07-09 21:20:49 +0200

    [upd] function event listener completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,11 @@ const button=document.getElementById("toggle-light");
 //fase2
 //gli assegno un event listener
 button.addEventListener(`click`,function(){
-  alert("hai cliccato il bottone")
+  let toggle=document.getElementById("light-bulb");
+  if(toggle.src.includes("white")){
+    toggle.src=toggle.src.replace("white","yellow")
+  }
+  else{
+    toggle.src=toggle.src.replace("yellow","white")
+  }
 })
