eacccc8e4862d3a9509f6f0cfb4814bead4496c6
DamianoMura <mura.damiano14@gmail.com>
2025-07-09 21:47:54 +0200

    [add] bonus content

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,18 @@ const button=document.getElementById("toggle-light");
 //gli assegno un event listener per click
 button.addEventListener(`click`,function(){
   let toggle=document.getElementById("light-bulb");//acquisiamo l'immagine
+  let button=document.getElementById("toggle-light");
   if(toggle.src.includes("white")){ //se nella url dell'immagine c'è scritto white lo cambiamo con yellow
     toggle.src=toggle.src.replace("white","yellow")
+    //cambiamo accendi in spegni
+    button.textContent=button.textContent.replace("accendi","spegni");
+    
   }
   else{//oppure cambiamo yellow con white
     toggle.src=toggle.src.replace("yellow","white")
+    //cambiamo spegni in accendi
+    button.textContent=button.textContent.replace("spegni","accendi");
+    
   }
-})
+
+});
