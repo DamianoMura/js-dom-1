05f910d3218d3002c2232d748b516932561ad3d5
DamianoMura <mura.damiano14@gmail.com>
2025-07-10 01:39:00 +0200

    [upd] perfectioning comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,18 @@ button.addEventListener(`click`,function(){
   let button=document.getElementById("toggle-light");
   if(toggle.src.includes("white")){ //se nella url dell'immagine c'è scritto white lo cambiamo con yellow
     toggle.src=toggle.src.replace("white","yellow")
-    //cambiamo accendi in spegni
+    //cambiamo accendi in spegni all'interno del bottone
     button.textContent=button.textContent.replace("accendi","spegni");
-    button.className=button.className.replace("danger","success");
+    //extra : cambiamo il colore del pulsante
+    button.className=button.className.replace("success","danger");
     
   }
   else{//oppure cambiamo yellow con white
     toggle.src=toggle.src.replace("yellow","white")
-    //cambiamo spegni in accendi
+    //cambiamo spegni in accendi all'interno del bottone
     button.textContent=button.textContent.replace("spegni","accendi");
-    button.className=button.className.replace("success","danger");
+     //extra : cambiamo il colore del pulsante
+    button.className=button.className.replace("danger","success");
     
   }
 
