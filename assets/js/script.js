776654a29025f25b0eca0692dd3f718b5f4b04fe
DamianoMura <mura.damiano14@gmail.com>
2025-07-09 21:08:04 +0200

    [upd] created button and successfully attached an event listener

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// fase 1
+//identifico il bottone
+const button=document.getElementById("toggle-light");
+//fase2
+//gli assegno un event listener
+button.addEventListener(`click`,function(){
+  alert("hai cliccato il bottone")
+})
